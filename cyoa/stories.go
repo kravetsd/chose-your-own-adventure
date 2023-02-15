66ffb1a269ed2eec3926e3d989d387976ab36a24
kravetsd <kdykrg@gmail.com>
2023-02-16 00:17:21 +0300

    some refactoring done

--- cyoa/stories.go
+++ cyoa/stories.go
@@ -23,18 +23,30 @@ func JsonStory(r io.Reader) (Story, error) {
 var myFuncMap = template.FuncMap{
 	// The name "title" is what the function will be called in the template text.
 	"join": strings.Join,
+	"plusOne": func(n int) int {
+		n += 1
+		return n
+	},
 }
 
 func (sh StoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//adding .Funcs(myFuncMap) to a template gives us a custom "join" function inside a template.
 	tp, err := template.New("story.html").Funcs(myFuncMap).ParseFiles("templates/story.html")
 	if err != nil {
 		log.Fatal("Parsing template:", err)
 	}
-	err = tp.Execute(w, sh.Story["intro"])
-	if err != nil {
-		log.Fatal("Executing template:", err)
+
+	if chapter, ok := sh.Story[strings.Split(r.URL.Path, "/")[1]]; ok {
+		err = tp.Execute(w, chapter)
+		if err != nil {
+			log.Fatal("Executing template:", err)
+		}
+	} else {
+		err = tp.Execute(w, sh.Story["intro"])
+		if err != nil {
+			log.Fatal("Executing template:", err)
+		}
 	}
-	//	log.Printf("%+v", sh)
 
 }
 
